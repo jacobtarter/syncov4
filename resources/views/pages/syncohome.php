d7d4a643187768ba3d9870cb7fe05414201b7424
jacobtarter <jtarter01@aurora.edu>
2016-04-19 23:43:43 -0500

    fixing bootstrap links to cdn

--- resources/views/pages/syncohome.php
+++ resources/views/pages/syncohome.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Load Bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 	<title>Synco Message Board</title>
 </head>
@@ -61,7 +63,9 @@
 <!-- Javascript libraries -->
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity=
+"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <!-- AngularJS Scripts -->
 <script src="<?= asset('angularjs/app.js') ?>"></script>
 <script src="<?= asset('angularjs/controllers/SyncoHomeController.js') ?>"></script>
