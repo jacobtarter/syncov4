06c3f40dc0de4344a00713099ee35e29f87ad759
jacobtarter <jtarter01@aurora.edu>
2016-04-20 00:33:34 -0500

    delete method sql problems

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -144,7 +144,7 @@ class PostController extends Controller
         
         print (json_encode($postsFinal));
         //echo json_encode($DATA);
-        }
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -208,7 +208,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request) 
+    public function destroy($pid) 
     {
     	$DEL = "DELETE FROM posts WHERE pid= '$pid' "
         DB::delete( "$DEL" );
