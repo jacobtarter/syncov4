ec51daf44f97767e07924a92593db257e14a4b0c
skippy12345 <jaredmurray012@gmail.com>
2016-04-20 16:16:11 -0500

    CSS TEST

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
 <head>
 <body>
 
-<link rel="stylesheet" href="{{ URL::asset('Synco.Css1.3.cs') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/Synco.Css1.3.cs') }}" />
 
 <p style="font-size: 24px"><b>Post New Comment!</b></p>
 <!--<script>
