24bd15cd62512da0e5497c052a47bd7555f965fb
jacobtarter <jtarter01@aurora.edu>
2016-04-21 07:19:41 -0500

    auth

--- resources/assets/js/controllers/userController.js
+++ resources/assets/js/controllers/userController.js
@@ -1,24 +1,19 @@
 app.controller('userController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 	angular.extend($scope, {
 		doLogin: function(loginForm) {
-			$http([
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				url: "synco.xyz/auth/",
-				method: "POST",
-				data: {
-					email: $scope.login.username,
-					password: $scope.login.password
-				}
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			url: 'synco.xyz/auth',
+			method: "POST",
+			data: {
+				email: $scope.login.username,
+				password: $scope.login.password
+			}
+		}).success(function(response)) {
+		console.log(response);
+		});
 
-			]).success(function(response) {
-				console.log(response);
-				$location.path('/');
-			}).error(function(data, status, headers) {
-				console.log(data, status, headers);
-				alert(data);
-			});
 		}
 	});
 }]);
