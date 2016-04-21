c84ff6b0f935ed2d51c7ecc4fbd2f40254e27f87
skippy12345 <jaredmurray012@gmail.com>
2016-04-20 19:31:40 -0500

    CSS

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -18,12 +18,12 @@ document.getElementById("login").innerHTML = "Hello $user->name";
 <h1> Test </h1>
 <div class = "commentTitleForm" >
 <p id="login"></p>
-    {!! Form::open(array('route' => 'posts.store', 'class' => 'test')) !!}
+    {!! Form::open(array('route' => 'posts.store')) !!}
      {{ Form::label('title', 'Title: ', 'class' => 'test') }}
     {{ Form::text('title', null) }}
-     {{ Form::label('posttext', "Post Text: " , 'class' => 'test') }}
-     {{ Form::textarea('ptext', null , 'class' => 'test') }}
-     {{ Form::submit('Create New Post' , 'class' => 'test') }}
+     {{ Form::label('posttext', "Post Text: ") }}
+     {{ Form::textarea('ptext', null) }}
+     {{ Form::submit('Create New Post') }}
 {!! Form::close() !!}
 </div>
 </head>
