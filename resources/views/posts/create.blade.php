26def1ecd24ae201beed5a23607e14af82f08302
skippy12345 <jaredmurray012@gmail.com>
2016-04-20 19:50:52 -0500

    CSS

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -18,7 +18,7 @@ document.getElementById("login").innerHTML = "Hello $user->name";
 <h1> Test </h1>
 <div class = "commentTitleForm" >
 <p id="login"></p>
-    {!! Form::open(array('route' => 'posts.store' , 'style' =>'background-color=red','border:solid gray 1px')) !!}
+    {!! Form::open(array('route' => 'posts.store' , 'style' =>'background-color=red') !!}
      {{ Form::label('title', 'Title: ') }}
     {{ Form::text('title', null) }}
      {{ Form::label('posttext', "Post Text: ") }}
