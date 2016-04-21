451e1079969198c55d2d472ba6049a37c8c92158
jacobtarter <jtarter01@aurora.edu>
2016-04-21 07:32:03 -0500

    auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
-use App\User;
-use \Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
