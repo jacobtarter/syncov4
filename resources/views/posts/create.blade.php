17f3da6b467163045bc4b204b3a90f02ed906cda
skippy12345 <jaredmurray012@gmail.com>
2016-04-20 19:29:43 -0500

    CSS

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -20,7 +20,7 @@ document.getElementById("login").innerHTML = "Hello $user->name";
 <p id="login"></p>
     {!! Form::open(array('route' => 'posts.store', 'class' => 'test')) !!}
      {{ Form::label('title', 'Title: ', 'class' => 'test') }}
-    {{ Form::text('title', null , 'class' => 'test') }}
+    {{ Form::text('title', null) }}
      {{ Form::label('posttext', "Post Text: " , 'class' => 'test') }}
      {{ Form::textarea('ptext', null , 'class' => 'test') }}
      {{ Form::submit('Create New Post' , 'class' => 'test') }}
