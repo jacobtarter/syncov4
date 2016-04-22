f0f09ee3faa02b0225f6cf62b28747e3c3c22f25
jacobtarter <jtarter01@aurora.edu>
2016-04-21 21:17:37 -0500

    gulp

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -51,24 +51,4 @@ synco.controller('mainController', ['$scope', '$http', function($scope, $http) {
 	
 
 }]);
-synco.controller('userController', ['$scope', '$http', '$location', function($scope, $http, $location) {
-	
-	angular.extend($scope, {
-		doLogin: function(loginForm) {
-			$http({	
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				url: baseUrl + 'auth',
-				method: "POST",
-				data: {
-					email: $scope.login.username,
-					password: $scope.login.password
-				}
-			}).success(function(response) {
-				console.log(response);
-			}).
-		}
-	});
-}]);
 //# sourceMappingURL=controllers.js.map
