f4679f57da17157481d094c0af35fd66ee42115d
jacobtarter <jtarter01@aurora.edu>
2016-04-21 19:53:10 -0500

    login

--- resources/assets/js/controllers/userController.js
+++ resources/assets/js/controllers/userController.js
@@ -1,20 +1,20 @@
 app.controller('userController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+	
 	angular.extend($scope, {
 		doLogin: function(loginForm) {
-			alert("thanks for logging in");
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			url: baseUrl + 'auth',
-			method: "POST",
-			data: {
-				email: $scope.login.username,
-				password: $scope.login.password
-			}
-		}).success(function(response)) {
-		console.log(response);
-		}).failure(function);
-
+			$http({	
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				url: baseUrl + 'auth',
+				method: "POST",
+				data: {
+					email: $scope.login.username,
+					password: $scope.login.password
+				}
+			}).success(function(response) {
+				console.log(response);
+			}).
 		}
 	});
 }]);
