21ec1c19030253f46dee85dd36ef1ef4c19d01c0
jacobtarter <jtarter01@aurora.edu>
2016-04-21 20:08:07 -0500

    login

--- public/build/js/controllers-d0f8074924.js
+++ public/build/js/controllers-d0f8074924.js
@@ -53,4 +53,24 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
 	
 
 }]);
+app.controller('userController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+	
+	angular.extend($scope, {
+		doLogin: function(loginForm) {
+			$http({	
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				url: baseUrl + 'auth',
+				method: "POST",
+				data: {
+					email: $scope.login.username,
+					password: $scope.login.password
+				}
+			}).success(function(response) {
+				console.log(response);
+			}).
+		}
+	});
+}]);
 //# sourceMappingURL=controllers.js.map
