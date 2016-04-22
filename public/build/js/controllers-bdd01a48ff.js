98067b7e295330e0879024c5518a6ced24a2fb93
jacobtarter <jtarter01@aurora.edu>
2016-04-22 02:38:45 -0500

    edit

--- public/build/js/controllers-bdd01a48ff.js
+++ public/build/js/controllers-bdd01a48ff.js
@@ -100,7 +100,7 @@ synco.controller('userController', ['$scope', '$http', '$location', function($sc
 			$scope.post = response;
 		});
 
-		alert($scope.post);
+		alert($scope.post[0]);
 
 
 		$location.path('/post/' + id );
