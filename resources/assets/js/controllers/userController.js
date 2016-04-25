08e1027e5979988187b14257ff6bf37c4b6f80cc
jacobtarter <jtarter01@aurora.edu>
2016-04-25 12:09:38 -0500

    editpost

--- resources/assets/js/controllers/userController.js
+++ resources/assets/js/controllers/userController.js
@@ -44,7 +44,7 @@ synco.controller('userController', ['$scope', '$http', '$location', function($sc
 		.success(function(response){
 			alert(baseUrl + api + "posts/" + id);
 			alert(response[0].about.title);
-			$scope.posts = response;
+			$scope.posts = response[0];
 		}).error(function(response) {
 			alert("error getting your json");
 		});
