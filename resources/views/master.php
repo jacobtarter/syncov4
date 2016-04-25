d795cc611c21c650c8186be403ed6bc0366b17dd
jacobtarter <jtarter01@aurora.edu>
2016-04-25 15:22:34 -0500

    master

--- resources/views/master.php
+++ resources/views/master.php
@@ -19,7 +19,7 @@
 	<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
 	<script type="text/javascript" src="bower_components/angular-route/angular-route.min.js"></script>
 	<script type="text/javascript" src="bower_components/angular-cookies/angular-cookies.min.js"></script>
-	<script type="text/javascript" src="js/app.js></script>
+	<script type="text/javascript" src="js/app.js"></script>
 	<script type="text/javascript" src="js/controllers.js"></script>
 
 </body>
