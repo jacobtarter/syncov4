aa85f957f10edd990860358787a6c2a5bea38c74
jacobtarter <jtarter01@aurora.edu>
2016-04-25 15:21:09 -0500

    master

--- resources/views/master.php
+++ resources/views/master.php
@@ -2,7 +2,7 @@
 <html ng-app="synco">
 <head>
 	<title>Synco</title>
-	<link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<script>var baseUrl = "{{ url('/') }}/";</script>
 	<script>var api = "api/v1/"; </script>
 </head>
@@ -16,11 +16,11 @@
 		<div ng-view></div>
 
 	</div>
-	<script type="text/javascript" src="{{asset('bower_components/angular/angular.min.js')}}"></script>
-	<script type="text/javascript" src="{{asset('bower_components/angular-route/angular-route.min.js')}}"></script>
-	<script type="text/javascript" src="{{asset('bower_components/angular-cookies/angular-cookies.min.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/controllers.js')}}"></script>
+	<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
+	<script type="text/javascript" src="bower_components/angular-route/angular-route.min.js"></script>
+	<script type="text/javascript" src="bower_components/angular-cookies/angular-cookies.min.js"></script>
+	<script type="text/javascript" src="js/app.js></script>
+	<script type="text/javascript" src="js/controllers.js"></script>
 
 </body>
 </html>
