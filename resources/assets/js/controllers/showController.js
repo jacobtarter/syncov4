ae9ec8ab3800de7e54d0dfeab435b6c1b9c8b578
jacobtarter <jtarter01@aurora.edu>
2016-04-26 00:26:49 -0500

    viewpost

--- resources/assets/js/controllers/showController.js
+++ resources/assets/js/controllers/showController.js
@@ -2,10 +2,16 @@ synco.controller('showController', ['$scope', '$http', '$location', '$routeParam
 
 		$scope.id = $routeParams.id;
 		$scope.post = null;
+		$scope.comments = [];
 
 		$http.get(baseUrl + api + "posts/" + $scope.id)
 		.success(function(response){
 			$scope.post= response;
+			angular.forEach(post.comments, function(nextComment) {
+				angular.forEach(nextComment.ctext, function(ctext)) {
+					$scope.comments.push(ctext);
+				}
+			})
 		}).error(function(response) {
 			alert("error getting your json");
 		});
